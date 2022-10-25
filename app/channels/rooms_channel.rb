856fdacfc5beff9dfa98927233562b44bd948e29
Gustavo <lgx.0097@gmail.com>
2022-10-25 18:17:29 -0300

    base game algorithm

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -1,11 +1,71 @@
 class RoomsChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
-
     room = Room.find(params[:id])
     stream_for room
 
-    RoomsChannel.broadcast_to(room, 'Assinatura realizada')
+    RoomsChannel.broadcast_to(room, "Conectado na sala: #{room.state}")
+
+    #1 esperando inicio
+
+    #2 Ler pergunta e esperando resposta
+
+    #3 mostrando parcial
+
+    #4 mostrando final
+  end
+
+
+  def init_game(data)
+    room = Room.find(params[:id])
+
+    #dando broadcast na perguntas e nas alternativas
+    room.update(state: :asking_questions, current_question: room.questions.first)
+    state = {
+      room: room.code,
+      state: room.state
+    }.to_json
+    RoomsChannel.broadcast_to(room, state)
+
+
+    room.questionaire.questions.each do |question|
+      #dando broadcast na perguntas e nas alternativas
+      question = {
+        question: question.question.first.text,
+        alternatives: question.question.first.alternatives
+      }.to_json
+      RoomsChannel.broadcast_to(room, question)
+
+      # esperando o tempo para as respostas
+      sleep(1.minute)
+
+      # mostrando o placar parcial
+      scoreboard = room.last_scoreboard.to_json
+      RoomsChannel.broadcast_to(room, question)
+
+      room.update(state: :asking_questions, current_question: room.current_question.next_question) if room.current_question.next_question.present?
+    end
+
+    # quando o chegar no final atualiza o status e mostra o score board
+    room.update(state: :ending, current_question: nil)
+    state = {
+      room: room.code,
+      state: room.state
+    }.to_json
+    RoomsChannel.broadcast_to(room, state)
+
+    scoreboard = room.last_scoreboard.to_json
+    RoomsChannel.broadcast_to(room, question)
+  end
+
+  def register_answer(data)
+    room = Room.find(params[:id])
+    user = user.find(data["user_id"])
+
+    return if not room.asking_questions?
+
+    # registra uma pergunta e retorna para o user
+    room.current_question.answers.create(user: user, answer: data['aswer'])
+    RoomsChannel.broadcast_to(user, { message: 'Aswer registred' }.to_json)
   end
 
   def unsubscribed
