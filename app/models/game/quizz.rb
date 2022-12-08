33414ec17da0d219317953784d05055f0c50f645
Gustavo <lgx.0097@gmail.com>
2022-12-08 18:00:36 -0300

    adding message_type to broadcast messages

--- app/models/game/quizz.rb
+++ app/models/game/quizz.rb
@@ -12,11 +12,14 @@ class Game::Quizz
     user.connect_to(room)
 
     broadcaster.send({
+      message_type: 'room_info',
+      user_count: room.connected_users.count,
+      state: room.state,
       message: "Conectado na sala - ESTADO: #{room.state}, CONECTADOS #{room.connected_users.count}"
     })
   end
 
-  def desconnect_from_room(user:)
+  def disconnect_from_room(user:)
     user.disconnect_from_room
   end
 
@@ -26,11 +29,13 @@ class Game::Quizz
     room.update!(state: :asking_questions, current_question: room.questions[0])
 
     broadcaster.send({
+      message_type: 'init_game',
       room: room.code,
       state: room.state
     })
 
     broadcaster.send({
+      message_type: 'sending_question',
       question: room.current_question,
       alternatives: room.current_question.alternatives
     })
@@ -41,7 +46,8 @@ class Game::Quizz
 
     user.answers.create!(room: room, question: room.current_question, alternative: alternative)
 
-    broadcaster.send({ 
+    broadcaster.send({
+      message_type: 'answer_confirmation',
       message: 'Aswer registred' 
     })
 
@@ -49,7 +55,10 @@ class Game::Quizz
   end
 
   def show_current_scoreboard
-    broadcaster.send(room.last_scoreboard)
+    broadcaster.send({
+      message_type: 'sending_scoreboard',
+      scoreboard: room.last_scoreboard
+    })
   end
 
   private
@@ -68,6 +77,7 @@ class Game::Quizz
     room.update(current_question: room.next_question)
 
     broadcaster.send({
+      message_type: 'sending_question',
       question: room.current_question,
       alternatives: room.current_question.alternatives
     })
@@ -77,8 +87,7 @@ class Game::Quizz
     room.update(state: :ending, current_question: nil)
 
     broadcaster.send({
-      room: room.code,
-      state: room.state
+      message_type: 'end_game'
     })
   end
 end
