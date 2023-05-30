baf648e9e8ec74899cb44c01e13e20aa40b38209
Gustavo <lgx.0097@gmail.com>
2023-05-30 17:27:22 -0300

    testing network disconnections

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,5 +1,24 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
     include ActionController::Cookies
+
+    after_subscribe :connection_monitor
+
+    CONNECTION_TIMEOUT = 10.seconds
+    CONNECTION_PING_INTERVAL = 5.seconds
+
+    periodically every: CONNECTION_PING_INTERVAL do
+      @driver&.ping
+      if Time.now - @_last_request_at > @_timeout
+        connection.disconnect
+      end
+    end
+
+    def connection_monitor
+      @_last_request_at ||= Time.now
+      @_timeout = CONNECTION_TIMEOUT
+      @driver = connection.instance_variable_get('@websocket').possible?&.instance_variable_get('@driver')
+      @driver.on(:pong) { @_last_request_at = Time.now }
+    end
   end
 end
