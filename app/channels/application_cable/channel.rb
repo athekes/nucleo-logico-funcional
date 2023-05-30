887a988c2c7bc2ef74e43c34528b18b8f219c0b3
Gustavo <lgx.0097@gmail.com>
2023-05-30 17:54:50 -0300

    quick fix

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -10,14 +10,13 @@ module ApplicationCable
     periodically every: CONNECTION_PING_INTERVAL do
       @driver&.ping
 
-      if Time.now - @_last_request_at > @_timeout
+      if Time.now - @_last_request_at > CONNECTION_TIMEOUT
         unsubscribed
       end
     end
 
     def connection_monitor
       @_last_request_at ||= Time.now
-      @_timeout = CONNECTION_TIMEOUT
       @driver = connection.instance_variable_get('@websocket').possible?&.instance_variable_get('@driver')
       @driver.on(:pong) { @_last_request_at = Time.now }
     end
