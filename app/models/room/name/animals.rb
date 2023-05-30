c9e1b89a6c4750dda20074d8fb0a79f84bb4a9da
Gustavo <lgx.0097@gmail.com>
2023-05-30 15:32:51 -0300

    room name 1 to 1000

--- app/models/room/name/animals.rb
+++ app/models/room/name/animals.rb
@@ -1,21 +1,22 @@
 class Room::Name::Animals 
-  LIST = [
-    "abelha",
-    "abutre",
-    "anta",
-    "aranha",
-    "arara",
-    "atum",
-    "bagre",
-    "barata",
-    "besouro",
-    "bode",
-    "boi",
-    "burro",
-    "cabra",
-    "cachorro",
-    "camelo",
-    "canguru",
-    "capivara"
-  ]
+  # LIST = [
+  #   "abelha",
+  #   "abutre",
+  #   "anta",
+  #   "aranha",
+  #   "arara",
+  #   "atum",
+  #   "bagre",
+  #   "barata",
+  #   "besouro",
+  #   "bode",
+  #   "boi",
+  #   "burro",
+  #   "cabra",
+  #   "cachorro",
+  #   "camelo",
+  #   "canguru",
+  #   "capivara"
+  # ]
+  LIST = (1..1000).to_a.map(&:to_s)
 end
