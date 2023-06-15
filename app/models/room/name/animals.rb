0b389135f0ea5ae3d6d0bf823603451c9b0f0b18
Gustavo <lgx.0097@gmail.com>
2023-06-15 12:27:31 -0300

    return to default room mode

--- app/models/room/name/animals.rb
+++ app/models/room/name/animals.rb
@@ -1,22 +1,22 @@
 class Room::Name::Animals 
-  # LIST = [
-  #   "abelha",
-  #   "abutre",
-  #   "anta",
-  #   "aranha",
-  #   "arara",
-  #   "atum",
-  #   "bagre",
-  #   "barata",
-  #   "besouro",
-  #   "bode",
-  #   "boi",
-  #   "burro",
-  #   "cabra",
-  #   "cachorro",
-  #   "camelo",
-  #   "canguru",
-  #   "capivara"
-  # ]
-  LIST = (1..1000).to_a.map(&:to_s)
+  LIST = [
+    "abelha",
+    "abutre",
+    "anta",
+    "aranha",
+    "arara",
+    "atum",
+    "bagre",
+    "barata",
+    "besouro",
+    "bode",
+    "boi",
+    "burro",
+    "cabra",
+    "cachorro",
+    "camelo",
+    "canguru",
+    "capivara"
+  ]
+  # LIST = (1..1000).to_a.map(&:to_s)
 end
